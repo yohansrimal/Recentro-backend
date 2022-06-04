98228c4d5a04b522661896488e3b07e10e862d79
yohansrimal <yohansrimalranasinghe@gmail.com>
2022-06-05 02:36:43 +0530

    updated panel members

--- models/PanelMember.model.js
+++ models/PanelMember.model.js
@@ -3,20 +3,20 @@ const mongoose = require("mongoose");
 const panelmemberSchema = new mongoose.Schema({
   staffID: {
     type: String,
-    require: true
+    require: true,
   },
   panelmemberID: {
     type: String,
-    require: true
+    require: true,
   },
   panelmemberName: {
     type: String,
-    require: true
+    require: true,
   },
   p_researchArea: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("Panelmember", panelmemberSchema);
